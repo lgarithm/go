a9337694f814ab362f521722ee369aef296bb959
lg <lg4869@outlook.com>
2018-09-11 12:33:15 +0800

    read config from file

--- tools/cmd/http-ping/http-ping.go
+++ tools/cmd/http-ping/http-ping.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"log"
+	"net/url"
+	"os"
+	"path"
+	"strings"
 	"time"
 
 	"github.com/lgarithm/go/control"
@@ -10,17 +15,16 @@ import (
 )
 
 var (
-	period  = flag.Duration("period", 5*time.Second, "")
+	defaultConfig = path.Join(os.Getenv("HOME"), ".http-ping")
+
+	period  = flag.Duration("period", 1*time.Second, "")
 	timeout = flag.Duration("timeout", 5*time.Second, "")
+	config  = flag.String("config", defaultConfig, "path to config file")
 )
 
 func main() {
-	urls := []string{
-		`http://www.qq.com`,
-		`http://www.baidu.com`,
-		`https://github.com/`,
-		`https://www.bing.com/`,
-	}
+	flag.Parse()
+	urls := loadConfig(*config)
 	var ps []*probe.Probe
 	for _, url := range urls {
 		p, err := probe.New(url, *timeout)
@@ -30,9 +34,44 @@ func main() {
 		}
 		ps = append(ps, p)
 	}
-	control.Periodically(*period, func() {
+	control.Periodically(time.Duration(len(ps))*(*period), func() {
 		for _, p := range ps {
 			p.Ping()
 		}
 	})
+}
+
+var defaultURLs = []string{
+	`http://www.qq.com`,
+	`http://www.baidu.com`,
+}
+
+func loadConfig(filename string) []string {
+	var urls []string
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Printf("Config %s not found, Using default config", filename)
+		return defaultURLs
+	}
+	defer f.Close()
+	br := bufio.NewReader(f)
+	for {
+		line, _, err := br.ReadLine()
+		if err != nil {
+			break
+		}
+		{
+			line := strings.TrimSpace(string(line))
+			if len(line) == 0 || strings.HasPrefix(line, "#") {
+				continue
+			}
+			u, err := url.Parse(line)
+			if err != nil {
+				log.Printf("invalid URL in config: %s, ignored", line)
+				continue
+			}
+			urls = append(urls, u.String())
+		}
+	}
+	return urls
 }
